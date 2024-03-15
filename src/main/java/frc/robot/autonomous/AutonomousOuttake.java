90bfbc8adeb5f5b20a5979893bee0badc828386f
skeole <skeole24@brophybroncos.org>
2024-03-15 01:55:43 -0700

    bug fix

--- src/main/java/frc/robot/autonomous/AutonomousOuttake.java
+++ src/main/java/frc/robot/autonomous/AutonomousOuttake.java
@@ -2,22 +2,16 @@ package frc.robot.autonomous;
 
 import edu.wpi.first.wpilibj2.command.*;
 import frc.robot.commands.activatedCommands.ShootingCommands;
-import frc.robot.commands.defaultCommands.TeleopSwerveRelativeDirecting;
 import frc.robot.subsystems.*;
 
-public class AutonomousOuttake extends SequentialCommandGroup {
+public class AutonomousOuttake extends ParallelRaceGroup {
 
     public AutonomousOuttake(Swerve swerve, Pivot pivot, Conveyor conveyor, Flywheel flywheel) {
         addRequirements(swerve, pivot, conveyor, flywheel);
 
         addCommands( // angle towards outtake
-            new ParallelDeadlineGroup(
-                new ParallelRaceGroup(
-                    new WaitCommand(8), 
-                    ShootingCommands.autonomousSpeaker(swerve, pivot, flywheel, conveyor)
-                ), 
-                new TeleopSwerveRelativeDirecting(swerve, () -> 0, () -> 0, () -> 0, () -> false, () -> -1, () -> 1, () -> true)
-            )
+            new WaitCommand(8), 
+            ShootingCommands.autonomousSpeaker(swerve, pivot, flywheel, conveyor)
         );
     }
     
