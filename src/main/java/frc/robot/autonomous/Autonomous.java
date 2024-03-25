37e415afd56bd8f9a702d713f9604ca73b90a484
Rohan Godha <rgodha24@brophybroncos.org>
2024-03-25 13:13:59 -0700

    amp paths

--- src/main/java/frc/robot/autonomous/Autonomous.java
+++ src/main/java/frc/robot/autonomous/Autonomous.java
@@ -1,18 +1,18 @@
 package frc.robot.autonomous;
 
 import com.pathplanner.lib.commands.PathPlannerAuto;
+
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
+import frc.robot.Variables;
 import frc.robot.subsystems.Swerve;
 
 public class Autonomous {
 
-    public static Command getAutonomousCommand(Swerve swerve) { // RIP Legacy Code </3
-
-        // String selectedAuto = SmartDashboard.getString("Auto Selector String", "NotAmpTriple");
-        //
-        // SmartDashboard.putString("Autonomous", (Variables.isBlueAlliance ? "Blue" : "Red") + " " + selectedAuto);
+    public static Command getAutonomousCommand(Swerve swerve) {
+        String selectedAuto = SmartDashboard.getString("Auto Selector String", "Center C3 C2 F1 C1");
+        SmartDashboard.putString("Autonomous", (Variables.isBlueAlliance ? "Blue" : "Red") + " " + selectedAuto);
         
-        var selectedAuto = "Center 3C0F";
         return new PathPlannerAuto(selectedAuto);
     }
 }
